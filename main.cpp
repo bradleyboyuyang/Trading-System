cb280cd357058936ff6b7dd90237cc8085c90b6f
bradleyboyuyang <bradleyboyuyang@gmail.com>
2024-01-02 16:52:52 -0500

    final version

--- main.cpp
+++ main.cpp
@@ -74,7 +74,7 @@ int main(int, char**){
 	// tickers
     vector<string> bonds = {"9128283H1", "9128283L2", "912828M80", "9128283J7", "9128283F5", "912810TW8", "912810RZ3"};
 	// generate price and orderbook data (specify random seed and number of data points)
-    genOrderBook(bonds, pricePath, marketDataPath, 39373, 2000);
+    genOrderBook(bonds, pricePath, marketDataPath, 39373, 5000);
     log(LogLevel::INFO, "Generating trade data...");
 	genTrades(bonds, tradePath, 39373);
     log(LogLevel::INFO, "Generating inquiry data...");
